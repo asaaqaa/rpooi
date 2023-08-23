2dd089ba44a319bd8c84603b21747cd8b1cde1cd
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-23 04:43:17 +0300

    Update skip.py

--- PrimeMusic/plugins/admins/skip.py
+++ PrimeMusic/plugins/admins/skip.py
@@ -19,7 +19,7 @@ SKIP_COMMAND = get_command("SKIP_COMMAND")
 
 
 @app.on_message(
-    filters.command(SKIP_COMMAND)
+    filters.command(SKIP_COMMAND,"")
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
